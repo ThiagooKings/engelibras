45cfa6449b3b13ec172e0a3822c6f9d1328a7d2c
ThiagooKings <thiagomreeis@gmail.com>
2021-04-17 19:08:05 -0300

    Primeira parte da página do dicionario e começõe da construção da tabela

--- src/pages/dicionario.tsx
+++ src/pages/dicionario.tsx
@@ -27,13 +27,60 @@ export default function Dicionario() {
                             <img src="img/layout/imgbannerdicionario.svg" />
                         </div>
                     </div>
+                </section>
+                {/* Seção conteudo dicionario */}
+                <section className={`${styles.containerFluid} ${styles.sectionBox}`}>
+                    <h1>
+                        <span className={styles.redText}>Dicio</span>
+                        <span className={styles.blueText}>Nário</span>
+                    </h1>
+                    <div className={styles.container}>
+                        <div className={styles.buscaeIdioma}>
+                            <div className={styles.buscaPalavra}>
+                                <form action="" method="post">
+                                    <input className={styles.inputBusca} type="text" placeholder="Pesquisar palavra"></input>
+                                    <button>Pesquisar</button>
+                                </form>
+                            </div>
+                            <div className={styles.selectIdioma}>
+                                <li>
+                                    <span> Portugês brasileiro </span> <img src="icons/bandeirabr.svg" />
+                                </li>
+                            </div>
+                        </div>
+                        <div className={styles.listaeConteudo}>
+                            <div className={styles.listaPalavras}>
+                                <table>
+                                    <tr>
+                                        <td> Fogo </td>
+                                    </tr>
+                                    <tr>
+                                        <td> Comburente </td>
+                                    </tr>
+                                    <tr>
+                                        <td> Extintor de Incendio </td>
+                                    </tr>
+                                    <tr>
+                                        <td> Fogo 2</td>
+                                    </tr>
+                                    <tr>
+                                        <td> Comburente 2</td>
+                                    </tr>
+                                </table>
 
+                            </div>
+                            <div className={styles.palavraeSignificado}>
+
+
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
             {/* Seção do Rodape */}
             <section>
                 <Rodape />
             </section>
-        </div>
+        </div >
     );
 }
